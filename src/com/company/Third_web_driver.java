afe206a6a32ee08c1fa7dae44faa190a5bc43277
pgarkaviy <pgarkaviy@gmail.com>
2015-02-05 09:25:36 +0200

    my first commit

---
+++ src/com/company/Third_web_driver.java
@@ -0,0 +1,40 @@
+package com.company;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * List correct translation
+ */
+public class Third_web_driver {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "D:\\My_projects\\aut\\chromedriver_win32\\chromedriver.exe");
+        WebDriver a = new ChromeDriver();
+
+        a.get("https://translate.google.com/#ru/en");
+
+        String[] input = {"товарищ", "привет", "пока"};
+        String[] expected = {"comrade", "hi", "as long as"};
+        int j = 0;
+        WebElement i = a.findElement(By.id("source"));
+        for (j=0; j < input.length; j++){
+            i.clear(); // Clear field
+            i.sendKeys(input[j]); // Print text
+            Thread.sleep(1000);
+            String result = a.findElement(By.id("result_box")).getText();
+            System.out.println(result);
+            if(result.equals(expected[j])){
+                System.out.println("Test passed");
+
+            }
+            else{
+                System.out.println("Test failed");
+            }
+        }
+
+        a.quit();
+
+    }
+}
